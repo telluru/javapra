db344ba314e6520d703140c17be57edd3f666670
telluru052 <crimson.lapiste@gmail.com>
2019-04-15 14:52:37 +0900

    Java４章２問目成功

---
+++ java4n2.java
@@ -0,0 +1,8 @@
+public class java4n2{
+  public static void main(String[] args){
+    int[] moneyList = {121902, 8302, 55100};
+    for(int i:moneyList){
+      System.out.println(i);
+    }
+  }
+}

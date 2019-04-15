3f1e7212af103b8b1e791861653d25b237468c8e
telluru052 <crimson.lapiste@gmail.com>
2019-04-15 16:44:03 +0900

    Java6章成功

---
+++ java6/comment/Kouhan.java
@@ -0,0 +1,11 @@
+package comment;
+public class Kouhan{
+  public static void callDeae(){
+    System.out.println("ええい小癪な。曲者だ！であえい！");
+  }
+  public static void showMondokoro() throws Exception{
+    System.out.println("飛車さん、核酸。もういいでしょう。");
+    System.out.println("この紋所が目に入らぬか！");
+    Zenhan.doTogame();
+  }
+}
---
+++ java6/comment/Zenhan.java
@@ -0,0 +1,10 @@
+package comment;
+public class Zenhan{
+  public static void doWarusa(){
+    System.out.println("黄な粉でござる。食えませんがの。");
+  }
+  public static void doTogame(){
+    System.out.println("この老いぼれの目はごまかせませんぞ。");
+  }
+
+}

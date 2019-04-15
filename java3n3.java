4021fdcda6861e4ed0a45b816e1162b6d94dfcc9
telluru052 <crimson.lapiste@gmail.com>
2019-04-15 13:53:36 +0900

    Java３章３問目成功

---
+++ java3n3.java
@@ -0,0 +1,17 @@
+public class java3n3{
+  public static void main(String[] args){
+    int seibetsu = 0;
+    int age = 3;
+    System.out.println("こんにちは");
+    switch(seibetsu){
+      case 0:
+        System.out.println("私は男です");
+        System.out.println(age+"歳です");
+        break;
+      case 1:
+        System.out.println("私は女です");
+        break;
+    }
+    System.out.println("よろしくお願いします");
+  }
+}
